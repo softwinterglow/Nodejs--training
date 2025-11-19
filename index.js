6e40ab9efa20ba2089e2e3b48c89a8677bf3aabe
sushant <bksushantofficial@gmail.com>
2025-11-19 08:13:31 +0545

    guess the output

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // console.log("hello world");
 
-let program = "Hello"
-console.log(program)
+// let program = "Hello"
+// console.log(program)
 
 
 
-const pi = 3.14
-console.log(pi);
+// const pi = 3.14
+// console.log(pi);
 
+
+
+
+// Guess The box
+let x = 10;
+if(true){
+    let x =20;
+    console.log("Inside",x)
+}
+console.log("Outside",x)
